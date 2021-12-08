64b31e88b90775e170c77b70f442dc140a84c760
Gbolahan Adegoke <adegokegbolahan24@gmail.com>
2021-12-08 14:11:41 -0800

    real-time updating

--- public/script.js
+++ public/script.js
@@ -20,24 +20,34 @@ if ("webkitSpeechRecognition" in window) {
 
   speechRecognition.onresult = (event) => {
     let interim_transcript = "";
-
+    let getWord = "";
+    var paragraph = document.getElementById("getWord");
+    
     for (let i = event.resultIndex; i < event.results.length; ++i) {
       if (event.results[i].isFinal) {
-        final_transcript += event.results[i][0].transcript;
+        final_transcript = event.results[i][0].transcript;
+        getWord = event.results[i][0].transcript;
+        var text = document.createTextNode(getWord + ",");
+        paragraph.appendChild(text);
       } else {
         interim_transcript += event.results[i][0].transcript;
       }
     }
-    document.querySelector("#final").innerHTML = final_transcript;
+    document.querySelector("#final").innerHTML = final_transcript ;
     document.querySelector("#interim").innerHTML = interim_transcript;
   };
 
   document.querySelector("#start").onclick = () => {
+    
     speechRecognition.start();
   };
   document.querySelector("#stop").onclick = () => {
+    var paragraph = document.getElementById("getWord");
+    var text = document.createTextNode(".");
+    paragraph.appendChild(text);
     speechRecognition.stop();
   };
 } else {
   console.log("Speech Recognition Not Available");
 }
+
